7e396d89bada4647c73ac08d88a49914f1b3a882
星河万里 <1214627679goddragon@gmail.com>
2025-12-12 16:18:44 +0800

    更新日志
    更新

--- Assets/Scripts/Component/Rocker.cs
+++ Assets/Scripts/Component/Rocker.cs
@@ -5,120 +5,117 @@ using UnityEngine.EventSystems;
 
 
 
-namespace Invariable
+public class Rocker : MonoBehaviour
 {
-    public class Rocker : MonoBehaviour
+    public RectTransform parent = null;
+    public RectTransform handle = null;
+
+    private bool isSetCurrPos = false;
+
+    private Action m_upFunc = null;
+    private Action m_downFunc = null;
+    private Action m_leftFunc = null;
+    private Action m_rightFunc = null;
+    private Action m_moveFunc = null;
+    private Action m_stayFunc = null;
+
+    private void Awake()
     {
-        public RectTransform parent = null;
-        public RectTransform handle = null;
+        parent = GetComponent<RectTransform>();
+    }
 
-        private bool isSetCurrPos = false;
-
-        private Action m_upFunc = null;
-        private Action m_downFunc = null;
-        private Action m_leftFunc = null;
-        private Action m_rightFunc = null;
-        private Action m_moveFunc = null;
-        private Action m_stayFunc = null;
-
-        private void Awake()
+    private void Update()
+    {
+        if (handle == null || handle.parent != transform)
         {
-            parent = GetComponent<RectTransform>();
+            return;
+        }
+        else if (!Input.GetMouseButton(0) && Input.touchCount <= 0)
+        {
+            gameObject.SetActive(false);
+            isSetCurrPos = false;
+            return;
         }
 
-        private void Update()
+        Vector2 point = Vector2.zero;
+
+        if (Input.GetMouseButton(0))
         {
-            if (handle == null || handle.parent != transform)
-            {
-                return;
-            }
-            else if (!Input.GetMouseButton(0) && Input.touchCount <= 0)
-            {
-                gameObject.SetActive(false);
-                isSetCurrPos = false;
-                return;
-            }
-
-            Vector2 point = Vector2.zero;
-
-            if (Input.GetMouseButton(0))
-            {
-                point = Input.mousePosition;
-            }
-            else if(Input.touchCount > 0)
-            {
-                point = Input.GetTouch(0).position;
-            }
-
-            RectTransformUtility.ScreenPointToLocalPointInRectangle(Utils.MainUIRoot, point, Utils.MainUICamera, out point);
-
-            if (!isSetCurrPos)
-            {
-                isSetCurrPos = true;
-                parent.anchoredPosition = point;
-                gameObject.SetActive(true);
-            }
-
-            Vector2 moveDir = point - parent.anchoredPosition;
-
-            if (moveDir.x > 0)
-            {
-                m_rightFunc?.Invoke();
-            }
-
-            if (moveDir.x < 0)
-            {
-                m_leftFunc?.Invoke();
-            }
-
-            if (moveDir.y > 0)
-            {
-                m_upFunc?.Invoke();
-            }
-
-            if (moveDir.y < 0)
-            {
-                m_downFunc?.Invoke();
-            }
-
-            if (moveDir == Vector2.zero)
-            {
-                m_stayFunc?.Invoke();
-            }
-            else
-            {
-                m_moveFunc?.Invoke();
-            }
+            point = Input.mousePosition;
+        }
+        else if (Input.touchCount > 0)
+        {
+            point = Input.GetTouch(0).position;
         }
 
-        public void SetUpMoveFunc(Action func)
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(Utils.MainUIRoot, point, Utils.MainUICamera, out point);
+
+        if (!isSetCurrPos)
         {
-            m_upFunc = func;
+            isSetCurrPos = true;
+            parent.anchoredPosition = point;
+            gameObject.SetActive(true);
         }
 
-        public void SetDownMoveFunc(Action func)
+        Vector2 moveDir = point - parent.anchoredPosition;
+
+        if (moveDir.x > 0)
         {
-            m_downFunc = func;
+            m_rightFunc?.Invoke();
         }
 
-        public void SetLeftMoveFunc(Action func)
+        if (moveDir.x < 0)
         {
-            m_leftFunc = func;
+            m_leftFunc?.Invoke();
         }
 
-        public void SetRightMoveFunc(Action func)
+        if (moveDir.y > 0)
         {
-            m_rightFunc = func;
+            m_upFunc?.Invoke();
         }
 
-        public void SetMoveFunc(Action func)
+        if (moveDir.y < 0)
         {
-            m_moveFunc = func;
+            m_downFunc?.Invoke();
         }
 
-        public void SetStayFunc(Action func)
+        if (moveDir == Vector2.zero)
         {
-            m_stayFunc = func;
+            m_stayFunc?.Invoke();
         }
+        else
+        {
+            m_moveFunc?.Invoke();
+        }
+    }
+
+    public void SetUpMoveFunc(Action func)
+    {
+        m_upFunc = func;
+    }
+
+    public void SetDownMoveFunc(Action func)
+    {
+        m_downFunc = func;
+    }
+
+    public void SetLeftMoveFunc(Action func)
+    {
+        m_leftFunc = func;
+    }
+
+    public void SetRightMoveFunc(Action func)
+    {
+        m_rightFunc = func;
+    }
+
+    public void SetMoveFunc(Action func)
+    {
+        m_moveFunc = func;
+    }
+
+    public void SetStayFunc(Action func)
+    {
+        m_stayFunc = func;
     }
 }
